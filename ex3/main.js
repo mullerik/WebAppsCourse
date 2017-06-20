adcd037d4ea89d79135be727b2b05b819b45d063
Arik Muller <arik@myfanzone.com>
2017-06-20 21:45:32 +0300

    Added hello.html file to be entry point for the app + added skeleton funcs for main.js

--- ex3/main.js
+++ ex3/main.js
@@ -11,8 +11,8 @@ var users = require('./routes/users');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -22,11 +22,56 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index);
+// app.use('/users', users);
+
+var userList = [];
+var itemList = [];
+
+// should add user/password to the user-list
+app.post('/register/:username/:password', function(req, res, next){
+  // TODO: Implement
+  null;
+});
+
+// should log in
+app.post('/login/:username/:password', function(req, res, next){
+    // TODO: Implement
+    null;
+});
+
+// Should add the json item to the items-list
+app.post('/item/', function(req, res, next){
+    // TODO: Implement
+    null;
+});
+
+// returns all the items as an array
+app.get('/items', function(req, res, next){
+    // TODO: Implement
+    null;
+});
+
+// returns the item with the right id or 404 if no such an item
+app.get('/item/:id', function(req, res, next){
+    // TODO: Implement
+    null;
+});
+
+// delete the item with the right id or 404 if no such an item
+app.delete('/item/:id', function(req, res, next){
+    // TODO: Implement
+    null;
+});
+
+// (item from type json in the HTTP body) overwrite the properties values of the item with the same id or 404 if no such an item
+app.use('/item/', function(req, res, next){
+    // TODO: Implement
+    null;
+});
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.put(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
