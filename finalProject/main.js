685eb741100b45a0c2af5bd7737fdc4c2947ace2
Arik Muller <arik@myfanzone.com>
2017-08-26 13:34:25 +0300

    Copied ex3 to finalProject to use as skeleton

---
+++ finalProject/main.js
@@ -0,0 +1,164 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// var index = require('./routes/index');
+// var users = require('./routes/users');
+
+var app = express();
+
+function renewCookie(req, res) {
+    var userCookie = req.cookies.uid;
+    if (userCookie) {
+        res.cookie('uid', userCookie.value, {maxAge: 3600000}); // Expires after 60 minutes
+    }
+}
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+// app.use('/', index);
+// app.use('/users', users);
+
+var uidCounter = 0;
+var userList = {};
+var itemList = {};
+
+
+// should add user/password to the user-list
+app.post('/register/:username/:password', function(req, res, next){
+    var username = req.params.username;
+    var password = req.params.password;
+    if (username in userList){
+        console.log("main.js: register: User already exists");
+        res.sendStatus(500);
+    } else {
+        uid = uidCounter;
+        uidCounter++;
+        userList[username] = {"uid": uid, "password": password};
+        console.log("main.js: register: Created user " + username + " uid: " + uid);
+        res.sendStatus(200);
+    }
+
+});
+
+// should log ins
+app.post('/login/:username/:password', function(req, res, next){
+    var username = req.params.username;
+    var password = req.params.password;
+    if (username in userList){
+        console.log("main.js: login: Logged in with user " + username);
+        if (userList[username]['password'] === password) {
+            res.cookie('uid', userList[username]['uid'], {maxAge: 3600000}); // Expires after 60 minutes
+            res.sendStatus(200);
+        } else {
+            console.log("main.js: login: password doesn't match user " + username);
+            res.sendStatus(403);
+        }
+    } else {
+        console.log("main.js: login: user " + username + " doesn't exist");
+        res.sendStatus(500);
+    }
+});
+
+
+// Validate only logged in users access the next routes
+app.use('/',function(req,res,next){
+    if(req.cookies.uid) {
+        renewCookie(req, res);
+        next();
+    }else{
+        res.json("You are unauthorized. Please login.");
+    }
+});
+
+// logout - basically delete login cookie
+app.post('/logout', function(req, res, next){
+    var userCookie = req.cookies.uid;
+    if (userCookie){
+        res.clearCookie('uid');
+        console.log("main.js: logout: Logged out successfully");
+        res.sendStatus(200);
+    } else {
+        console.log("main.js: logout: No cookie found, user is already logged out");
+        res.sendStatus(500);
+    }
+});
+
+// Should add the json item to the items-list
+app.post('/item/', function(req, res, next){
+    itemList[parseInt(req.body.id)] = req.body.data;
+    res.sendStatus(200);
+});
+
+// returns all the items as an array
+app.get('/items', function(req, res, next){
+    res.json(Object.keys(itemList).map(function(itemID){
+        return {"id": itemID, "data": itemList[itemID]};
+    }));
+});
+
+// returns the item with the right id or 404 if no such an item
+app.get('/item/:id', function(req, res, next){
+    var itemID = parseInt(req.params.id);
+    if (itemID in itemList){
+        res.json({"id": itemID, "data": itemList[itemID]})
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+// delete the item with the right id or 404 if no such an item
+app.delete('/item/:id', function(req, res, next){
+    var itemID = parseInt(req.params.id);
+    if (itemID in itemList){
+        delete itemList[itemID];
+        res.sendStatus(200);
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+// (item from type json in the HTTP body) overwrite the properties values of the item with the same id or 404 if no such an item
+app.put('/item/', function(req, res, next){
+    renewCookie(req, res);
+    var itemID = parseInt(req.body.id);
+    var newItemData = req.body.data;
+    if (itemID in itemList){
+        itemList[itemID] = newItemData;
+    } else {
+        res.sendStatus(404);
+    }
+});
+
+// catch 404 and forward to error handler
+app.put(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ finalProject/public/javascripts/script.js
@@ -0,0 +1,190 @@
+var login = document.getElementById("loginBtn");
+login.addEventListener('click', loginAjax);
+var register = document.getElementById("registerBtn");
+register.addEventListener('click', registerAjax);
+var logout = document.getElementById("logoutBtn");
+logout.addEventListener('click', logoutAjax);
+var postItem = document.getElementById("postBtn");
+postItem.addEventListener('click', addItem);
+var deleteItem = document.getElementById("removeBtn");
+deleteItem.addEventListener('click', removeItem);
+
+// List of all items
+var allTasks = {
+    1: "CHECK YOUR PASSPORT AND APPLY FOR ANY NECESSARY VISAS",
+    2: "GET VACCINATED AND STOCK UP ON MEDICINE",
+    3: "CHECK FOR TRAVEL WARNINGS/ADVISORIES AND REGISTER YOUR TRIP.",
+    4: "PREPARE YOUR FINANCES",
+    5: "MAKE COPIES OF TRAVEL DOCUMENTS",
+    6: "OBTAIN AN INTERNATIONAL DRIVING PERMIT",
+    7: "GET ALL THE NECESSARY GEAR FOR YOUR ELECTRONICS",
+    8: "LEARN KEY PHRASES IN THE LOCAL LANGUAGE",
+    9: "RESEARCH ENTRANCE AND EXIT FEES",
+    10: "BUY HEALTH AND TRAVEL INSURANCE"
+};
+
+// Decide which error to display according the status code
+function displayErrorMessage(status){
+    // Clear error messages
+    document.getElementById("warningMessage403").style.display = "none";
+    document.getElementById("warningMessage500").style.display = "none";
+    if (status == 403) {
+        document.getElementById("warningMessage403").style.display = "block";
+    } else if (status == 500) {
+        document.getElementById("warningMessage500").style.display = "block";
+    }
+}
+
+// get table id and delete all inner content
+function resetTable(tableID){
+    var table = document.getElementById(tableID);
+    table.innerHTML = "";
+    var row = table.insertRow();
+    var headerCell1 = row.insertCell(0);
+    headerCell1.outerHTML = "<th>ID</th>";
+    var headerCell2 = row.insertCell(1);
+    headerCell2.outerHTML = "<th>Description</th>";
+}
+// decide which page to display
+function togglePages() {
+    if (document.getElementById("loginPage").style.display === "none") {
+        document.getElementById("loginPage").style.display = "inline";
+        document.getElementById("contentPage").style.display = "none";
+        document.getElementsByTagName("body")[0].style.background = 'darkslategrey';
+    } else {
+        document.getElementById("loginPage").style.display = "none";
+        document.getElementById("contentPage").style.display = "inline";
+        document.getElementsByTagName("body")[0].style.background = 'url("/public/images/pexels-photo-199898.jpeg")';
+    }
+}
+
+// Send register request to the server
+function registerAjax() {
+    var user = document.getElementById("username");
+    var password = document.getElementById("password");
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log("Registered user: " + user.value);
+        }
+    };
+    xhttp.open("POST", "/register/" + user.value + "/" + password.value, true);
+    xhttp.send();
+}
+
+// Send login request to the server
+function loginAjax() {
+    var user = document.getElementById("username");
+    var password = document.getElementById("password");
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+            if (this.status == 200) {
+                togglePages();
+                console.log("Logged in with user: " + user.value);
+                var oldText = document.getElementById("connectedWelcome").innerText;
+                var newText = oldText.replace("user", user.value);
+                document.getElementById("connectedWelcome").innerText = newText;
+                refreshItemList();
+            } else if (this.status == 403 || this.status == 500) {
+                displayErrorMessage(this.status)
+            }
+        }
+    };
+    xhttp.open("POST", "/login/" + user.value + "/" + password.value, true);
+    xhttp.send();
+}
+
+function logoutAjax() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            // No need to toggle pages anymore, since we're using reload later on
+            // togglePages();
+            console.log("Logged out");
+        }
+    };
+    xhttp.open("POST", "/logout", true);
+    xhttp.send();
+    location.reload();
+}
+
+function refreshItemList(){
+    var table = document.getElementById("doneTasksTable");
+    resetTable("doneTasksTable");
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var respObj = JSON.parse(this.responseText);
+            for (var i in respObj) {
+                var curItemID = respObj[i].id;
+                var curItem = respObj[i].data;
+                var row = table.insertRow();
+                var cell1 = row.insertCell(0);
+                var cell2 = row.insertCell(1);
+                cell1.innerText = curItemID;
+                cell2.innerText = curItem;
+            }
+            allAvailableItems(respObj)
+        }
+    };
+    xhttp.open("GET", "/items", true);
+    xhttp.send();
+}
+
+// Convert array of tasks to object by IDs
+function arrayToObject(arr){
+    var tempObj = {};
+    for(var i in arr){
+        tempObj[arr[i]["id"]] = arr[i]["data"];
+    }
+    return tempObj;
+}
+
+function allAvailableItems(userTasks){
+    userTasks = arrayToObject(userTasks);
+    var table = document.getElementById("allTasksTable");
+    resetTable("allTasksTable");
+    Object.keys(allTasks).forEach(function(key, index){
+        // Don't show tasks user already finished
+        if (key in userTasks)
+            return;
+
+        // Otherwise - add them to list
+        var curItem = allTasks[key];
+        var row = table.insertRow();
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        cell1.innerText = key;
+        cell2.innerText = curItem;
+    });
+}
+
+function addItem() {
+    var itemID = document.getElementById("itemInput");
+    if (!(itemID.value in allTasks)) {
+        console.log("Error: item id is not in all items");
+        return;
+    }
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            refreshItemList();
+        }
+    };
+    xhttp.open("POST", "/item/", true);
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("id=" + itemID.value + "&data=" + allTasks[itemID.value]);
+}
+
+function removeItem() {
+    var itemID = document.getElementById("itemInput");
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            refreshItemList();
+        }
+    };
+    xhttp.open("DELETE", "/item/" + itemID.value, true);
+    xhttp.send();
+}
